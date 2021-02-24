8f494cce5b7b52af1019f8fc0fd155b907dffdec
CaryllOrtigue <77064960+CaryllOrtigue@users.noreply.github.com>
2021-02-24 06:13:58 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-                <title>homepage</title>
+                <title> homepage </title>
 </head>
 <body background=”hope.jpeg”>
 <font face=”Cooper Black”, size=”24”, color=”white”>
