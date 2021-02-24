0b0299f2e42d8a755f92c80b5bde1525a1698126
CaryllOrtigue <77064960+CaryllOrtigue@users.noreply.github.com>
2021-02-24 07:11:42 -0800

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,11 @@
 <head>
                 <title> homepage </title>
 </head>
-<body background=”hope.jpg”>
-<font color=”white” size=”24” face=”verdana”>
-<align=”center”>
+<body background="hope.jpg">
+<font color="white" size="24" face="verdana">
+<align="center">
 TODAY AND TOMORROW
   </center>
 </font>
-</p>
 </body>
 </hmtl>
